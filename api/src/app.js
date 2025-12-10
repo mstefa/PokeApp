f2de114d713514ffe877055245ea9c0d71648049
Matias Stefanutti <mstefanutti24@gmail.com>
2025-12-10 16:44:31 -0300

    feat: update dependencies versions

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 
@@ -8,14 +6,15 @@ require('./db.js');
 
 const server = express();
 
-server.name = 'API';
+server.name = 'POKE-API';
 
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json({ limit: '50mb' }));
 server.use(morgan('dev'));
+
+// CORS middleware
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -24,14 +23,12 @@ server.use((req, res, next) => {
 
 server.use('/', routes);
 
-// Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+// Error handling middleware
+server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
   res.status(status).send(message);
 });
 
-//==================================
-// server.listen(3000)
 module.exports = server;
